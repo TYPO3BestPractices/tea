a308257d74d63d0b5169871a7ce8c645179890fc
Eike Starkmann <eike.starkmann@posteo.de>
2025-02-03 17:17:16 +0100

    [TASK] remove no longer needed php-ex commands and PHP_INI_SCAN_DIR
    Part of #1354, #1579

--- .gitlab/build/docker_install.sh
+++ .gitlab/build/docker_install.sh
@@ -3,10 +3,3 @@
 [[ ! -e /.dockerenv ]] && exit 0
 
 set -xe
-
-apk add parallel
-apk add icu-dev
-docker-php-ext-configure intl
-docker-php-ext-install intl
-docker-php-ext-enable intl
-docker-php-ext-install pdo_mysql mysqli
