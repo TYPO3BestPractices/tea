97bcc2923b751a849e92503b732fa904a9dc4fe0
Karsten Nowak <nowak@undkonsorten.com>
2025-07-07 16:47:24 +0200

    [TASK] First steps to switch to CType for plugins
    Related: #1332

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,49 +1,64 @@
 <?php
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') || die();
 
 call_user_func(
     static function (): void {
-        // This makes the plugin selectable in the BE.
-        $indexPluginSignature = ExtensionUtility::registerPlugin(
+        /**
+         * Add new select group for content element
+         */
+        ExtensionManagementUtility::addTcaSelectItemGroup(
+            'tt_content',
+            'CType',
+            'tea',
+            'LLL:EXT:tea/Resources/Private/Language/locallang_db.xlf:tx_tea_domain_model_tea.pluginGroup',
+            'after:default'
+        );
+
+        /**
+         * Register all plugins
+         */
+        $plugins = ['tea_index','tea_show','tea_front_end_editor'];
+
+        foreach ($plugins as $contentType) {
+            // This makes the plugin selectable in the BE.
+            ExtensionUtility::registerPlugin(
             // extension name, matching the PHP namespaces (but without the vendor)
-            'Tea',
-            // arbitrary, but unique plugin name (not visible in the BE)
-            'TeaIndex',
-            // plugin title, as visible in the drop-down in the BE
-            'LLL:EXT:tea/Resources/Private/Language/locallang.xlf:plugin.tea_index',
-            // the icon visible in the drop-down in the BE
-            'EXT:tea/Resources/Public/Icons/Extension.svg'
-        );
+                'Tea',
+                // arbitrary, but unique plugin name (not visible in the BE)
+                GeneralUtility::underscoredToUpperCamelCase($contentType),
+                // plugin title, as visible in the drop-down in the BE
+                'LLL:EXT:tea/Resources/Private/Language/locallang.xlf:plugin.' . $contentType,
+                // the icon visible in the drop-down in the BE
+                'EXT:tea/Resources/Public/Icons/Extension.svg',
+                'tea'
+            );
 
-        // These two commands add the flexform configuration for the plugin.
-        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$indexPluginSignature] = 'pi_flexform';
+        }
+
+
+        // Add the FlexForm to the show item list
+        ExtensionManagementUtility::addToAllTCAtypes(
+            'tt_content',
+            '--div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.plugin, pi_flexform',
+            'tea_teaindex',
+            'after:palette:headers'
+        );
+        // Add the flexform configuration for the TeaIndex plugin.
         ExtensionManagementUtility::addPiFlexFormValue(
-            $indexPluginSignature,
-            'FILE:EXT:tea/Configuration/FlexForms/TeaIndex.xml'
+            '*',
+            'FILE:EXT:tea/Configuration/FlexForms/TeaIndex.xml',
+            'tea_teaindex'
         );
 
-        $showPluginSignature = ExtensionUtility::registerPlugin(
-            'Tea',
-            'TeaShow',
-            'LLL:EXT:tea/Resources/Private/Language/locallang.xlf:plugin.tea_show',
-            'EXT:tea/Resources/Public/Icons/Extension.svg'
-        );
+        /**
+         * Register TeaIndex as "Insert Record"
+         */
+        ExtensionManagementUtility::addToInsertRecords('tea_index');
 
-        $editorPluginSignature = ExtensionUtility::registerPlugin(
-            'Tea',
-            'TeaFrontEndEditor',
-            'LLL:EXT:tea/Resources/Private/Language/locallang.xlf:plugin.tea_frontend_editor',
-            'EXT:tea/Resources/Public/Icons/Extension.svg'
-        );
-
-        // This removes the default controls from the plugins.
-        $controlsToRemove = 'recursive,pages';
-        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$indexPluginSignature] = $controlsToRemove;
-        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$showPluginSignature] = $controlsToRemove;
-        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$editorPluginSignature] = $controlsToRemove;
     }
 );
