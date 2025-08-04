bb7061933cccce9a83a82b610c3c0954fdd9c8ee
Karsten Nowak <nowak@undkonsorten.com>
2025-08-04 08:44:38 +0200

    [TASK] Change the fixtures
    locally all tests are green

---
+++ Classes/Upgrades/ListTypeToCTypeUpdate.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TTN\Tea\Upgrades;
+
+use TTN\Tea\Upgrades\AbstractListTypeToCTypeUpdate;
+use TYPO3\CMS\Install\Attribute\UpgradeWizard;
+
+#[UpgradeWizard('teaListTypeToCTypeUpdate')]
+final class ListTypeToCTypeUpdate extends AbstractListTypeToCTypeUpdate
+{
+    protected function getListTypeToCTypeMapping(): array
+    {
+        return [
+            'tea_teaindex' => 'tea_teaindex',
+            'tea_teashow' => 'tea_teashow',
+            'tea_teafront_end_editor' => 'tea_teafront_end_editor',
+        ];
+    }
+
+    public function getTitle(): string
+    {
+        return 'Migrates tea extension plugins';
+    }
+
+    public function getDescription(): string
+    {
+        return 'Migrates tea_index, tea_show, tea_front_end_editor from list_type to CType.';
+    }
+}
