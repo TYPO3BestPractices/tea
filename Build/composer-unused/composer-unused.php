5823cfa6335fa466b545cbfc6a228762afc1baff
Eike Starkmann <eike.starkmann@posteo.de>
2026-03-02 16:37:59 +0100

    [TASK] small cleanup
    Related:  #1332

--- Build/composer-unused/composer-unused.php
+++ Build/composer-unused/composer-unused.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 use ComposerUnused\ComposerUnused\Configuration\Configuration;
 use ComposerUnused\ComposerUnused\Configuration\NamedFilter;
 
-
 return static function (Configuration $config): Configuration {
     $config->addNamedFilter(NamedFilter::fromString('typo3/cms-fluid'));
     return $config;
