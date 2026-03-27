64627fcfb3af1a69f0f1d43edb84c0fcc9e11589
Eike Starkmann <eike.starkmann@undkonsorten.com>
2026-03-27 13:17:07 +0100

    [TASK] Allow installations with TYPO3 13 (#2026)
    Resolves: #2019
    
    ---------
    
    Co-authored-by: Felix Althaus <felix.althaus@undkonsorten.com>

--- Build/rector/config.php
+++ Build/rector/config.php
@@ -56,7 +56,6 @@ return RectorConfig::configure()
         Typo3SetList::GENERAL,
 
         Typo3LevelSetList::UP_TO_TYPO3_12,
-        // Typo3LevelSetList::UP_TO_TYPO3_13,
     ])
     // To have a better analysis from PHPStan, we teach it here some more things
     ->withPHPStanConfigs([
@@ -69,7 +68,7 @@ return RectorConfig::configure()
     ->withImportNames(true, true, false)
     ->withConfiguredRule(ExtEmConfRector::class, [
         ExtEmConfRector::PHP_VERSION_CONSTRAINT => '8.1.0-8.5.99',
-        ExtEmConfRector::TYPO3_VERSION_CONSTRAINT => '12.4.41-12.4.99',
+        ExtEmConfRector::TYPO3_VERSION_CONSTRAINT => '12.4.41-13.4.99',
         ExtEmConfRector::ADDITIONAL_VALUES_TO_BE_REMOVED => [],
     ])
     ->withSkip([
