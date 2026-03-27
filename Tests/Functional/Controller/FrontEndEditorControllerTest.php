b81b0dde51cc22f8d73253ab77faefcec20698b2
Eike Starkmann <eike.starkmann@undkonsorten.com>
2026-03-27 12:41:20 +0100

    [TASK] TYPO3 13 renders the input without "/>" but with ">"
    Related: #2024

--- Tests/Functional/Controller/FrontEndEditorControllerTest.php
+++ Tests/Functional/Controller/FrontEndEditorControllerTest.php
@@ -181,10 +181,11 @@ final class FrontEndEditorControllerTest extends AbstractFrontendControllerTestC
             'tx_tea_teafrontendeditor[tea]' => (string)self::UID_OF_TEA,
         ]);
 
-        self::assertStringContainsString(
-            '<input type="hidden" name="tx_tea_teafrontendeditor[tea][__identity]" value="1" />',
-            $html,
+        self::assertTrue(
+            str_contains($html, '<input type="hidden" name="tx_tea_teafrontendeditor[tea][__identity]" value="1" />') ||
+            str_contains($html, '<input type="hidden" name="tx_tea_teafrontendeditor[tea][__identity]" value="1" >')
         );
+
         self::assertStringContainsString('Godesberger Burgtee', $html);
     }
 
