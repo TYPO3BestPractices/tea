331c11249fae903cffacc7782ae22f33a79c5765
Heiko Hardt <heiko@hardt.dev>
2026-03-28 15:53:01 +0100

    [TASK] Rename method in ExtensionTest for clarity and simplify implementation
    Adjusted `checkIfCurrentVersionIsSupported` to `currentVersionIsSupported` and removed verbose inline comments for cleaner and more concise code.
    
    Related: #2035

--- Tests/Unit/Environment/ExtensionTest.php
+++ Tests/Unit/Environment/ExtensionTest.php
@@ -6,19 +6,17 @@ namespace TTN\Tea\Tests\Unit\Environment;
 
 use PHPUnit\Framework\Attributes\CoversNothing;
 use PHPUnit\Framework\Attributes\Test;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
 
 #[CoversNothing]
 final class ExtensionTest extends UnitTestCase
 {
     #[Test]
-    public function checkIfCurrentVersionIsSupported(): void
+    public function currentVersionIsSupported(): void
     {
-        // Define the supported TYPO3 versions
         $supportedVersions = [12, 13];
-        // Get the current TYPO3 version
-        $currentVersion = (new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion();
-        // Check if the current TYPO3 version is in the array
+        $currentVersion = (new Typo3Version())->getMajorVersion();
         self::assertContains(
             $currentVersion,
             $supportedVersions
