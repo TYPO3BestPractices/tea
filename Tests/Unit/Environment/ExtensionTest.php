7d816f81ae164629ad744c357cf83ab8d832d1b7
Heiko Hardt <heiko@hardt.dev>
2026-03-28 15:44:29 +0100

    [TASK] Add unit test for supported TYPO3 versions in ExtensionTest
    Introduced a unit test to verify if the current TYPO3 version is supported by the extension.
    
    Related: #2035

---
+++ Tests/Unit/Environment/ExtensionTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TTN\Tea\Tests\Unit\Environment;
+
+use PHPUnit\Framework\Attributes\Test;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+final class ExtensionTest extends UnitTestCase
+{
+    #[Test]
+    public function checkIfCurrentVersionIsSupported(): void
+    {
+        // Define the supported TYPO3 versions
+        $supportedVersions = [12, 13];
+        // Get the current TYPO3 version
+        $currentVersion = (new \TYPO3\CMS\Core\Information\Typo3Version())->getMajorVersion();
+        // Check if the current TYPO3 version is in the array
+        self::assertContains(
+            $currentVersion,
+            $supportedVersions
+        );
+    }
+}
