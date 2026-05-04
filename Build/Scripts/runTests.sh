59079383fb654d58da09b178a5dc72034d097f1f
Anna Reiter <anna.reiter@oliverklee.de>
2026-05-04 15:01:02 +0200

    [TASK] Add `phpCsFixer` into a Bash command (#2113)

--- Build/Scripts/runTests.sh
+++ Build/Scripts/runTests.sh
@@ -171,6 +171,15 @@ cleanRenderedDocumentationFiles() {
     echo "done"
 }
 
+ phpCsFixer() {
+    if [ -n "${CGLCHECK_DRY_RUN}" ]; then
+        CGLCHECK_DRY_RUN="--dry-run --diff"
+    fi
+    COMMAND="php .Build/bin/php-cs-fixer fix -v ${CGLCHECK_DRY_RUN} --config=Build/php-cs-fixer/config.php"
+    ${CONTAINER_BIN} run ${CONTAINER_COMMON_PARAMS} --name phpCsFixer-${SUFFIX} ${IMAGE_PHP} ${COMMAND}
+    return
+ }
+
 loadHelp() {
     # Load help text into $HELP
     read -r -d '' HELP <<EOF
@@ -680,11 +689,7 @@ case ${TEST_SUITE} in
         SUITE_EXIT_CODE=$?
         ;;
     phpCsFixer)
-        if [ -n "${CGLCHECK_DRY_RUN}" ]; then
-            CGLCHECK_DRY_RUN="--dry-run --diff"
-        fi
-        COMMAND="php .Build/bin/php-cs-fixer fix -v ${CGLCHECK_DRY_RUN} --config=Build/php-cs-fixer/config.php"
-        ${CONTAINER_BIN} run ${CONTAINER_COMMON_PARAMS} --name phpcsfixer-${SUFFIX} ${IMAGE_PHP} ${COMMAND}
+        phpCsFixer
         SUITE_EXIT_CODE=$?
         ;;
     phpmd)
