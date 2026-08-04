c0c316a9eb228431a7d03f3c86417f650e21af77
Oliver Klee <typo3-coding@oliverklee.de>
2026-08-04 16:56:33 +0200

    [TASK] Also run the unit tests with TYPO3 14LTS (#2266)
    Part of #2253

--- Tests/Unit/VersionCompatibilityTest.php
+++ Tests/Unit/VersionCompatibilityTest.php
@@ -15,7 +15,7 @@ final class VersionCompatibilityTest extends UnitTestCase
     #[Test]
     public function currentVersionIsSupported(): void
     {
-        $supportedVersions = [13];
+        $supportedVersions = [13, 14];
         $currentVersion = (new Typo3Version())->getMajorVersion();
         self::assertContains(
             $currentVersion,
